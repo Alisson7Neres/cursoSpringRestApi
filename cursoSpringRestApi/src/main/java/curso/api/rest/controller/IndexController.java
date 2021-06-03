637826d893241c4cd4fa9ab1215399220fc4b6e3
Alisson Neres <neres@pop-os.localdomain>
2021-06-03 08:30:34 -0300

    adicionando bd do Heroku

--- cursoSpringRestApi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursoSpringRestApi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,12 +14,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
 import curso.api.rest.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "*")
 @RestController // Arquitetura REST
 @RequestMapping(value = "/usuario")
 public class IndexController {
