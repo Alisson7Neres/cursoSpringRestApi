6c1a68bde6ed358345cf065c7ec968b0f1b73b3c
neres <neres@pop-os.localdomain>
2021-06-11 11:36:49 -0300

    Senhas criptografadas e login único.

--- cursoSpringRestApi/src/main/java/curso/api/rest/controller/IndexController.java
+++ cursoSpringRestApi/src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,7 +38,7 @@ public class IndexController {
 		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
 	
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@GetMapping(value = "/codigo", produces = "application/json")
 	public ResponseEntity codigo() {
 		return new ResponseEntity("Teste", HttpStatus.OK);
@@ -56,18 +57,27 @@ public class IndexController {
 		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
+		
+		String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhaCriptografada);
 		Usuario usuarioSalvo =  usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
 	
-	@PutMapping(value = "/{id}", produces = "application/json")
-	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario,
-			@PathVariable(value = "id") Long id){
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
 		
 		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
 			usuario.getTelefones().get(pos).setUsuario(usuario);
 		}
 		
+		Usuario userTemp = usuarioRepository.findUserByLogin(usuario.getLogin());
+		
+		//Senhas diferentes
+		if(!userTemp.getSenha().equals(usuario.getSenha())) {
+			String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+			usuario.setSenha(senhaCriptografada);
+		}
 		Usuario usuarioAtualizar = usuarioRepository.save(usuario);
 		
 		return new ResponseEntity<Usuario>(usuarioAtualizar, HttpStatus.OK);
